6c3a33e0873a02ba591ed5b9a823a73f1e633332
Fazal <fazal.rehman@sabre.com>
2025-01-14 13:26:26 +0500

    update section 1 and git added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'NG-deep-dive';
+  counter: number = 0;
+
+
+  data = {
+    tittle : 'Angular Deep Dive - Beginner to Advanced (Angular 19)'
+  }
+
+
+  KeyType(newTittle:string) {
+    this.data.tittle = newTittle;
+  }
+
+
+  LogoTouch() {
+    this.counter++;
+    console.log("Clicked Time: ", this.counter)
+  }
+
 }
